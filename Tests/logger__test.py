e60d1c9aad96b7bc920c1883c685e09f2c3c2422
thefunnyfunyan <brando15@ksu.edu>
2016-10-15 09:11:25 -0500

    Finshed trace logging and tests

--- Tests/logger__test.py
+++ Tests/logger__test.py
@@ -4,11 +4,15 @@ from logger import logger
 class LoggerTest(unittest.TestCase):
     def setUp(self):
         self.testLogger = logger()
-        self.errorFile = 'Logs/ErrorLog.txt'
-        f = open("Logs/ErrorLog.txt", 'w')
+        self.errorFile = "Logs/ErrorLog.txt"
+        self.traceFile = "Logs/TraceLog.txt"
+        f = open(self.errorFile, 'w')
         f.write("")
         f.close()
-        
+        f = open(self.traceFile, 'w')
+        f.write("")
+        f.close()
+
     def test_ShouldInitializeLogger(self):
         self.assertNotEqual(self.testLogger, None)
 
@@ -31,7 +35,24 @@ class LoggerTest(unittest.TestCase):
         self.assertEqual(f.readline(), "In Class error Class\n")
         self.assertEqual(f.readline(), "<---ERROR--->\n")
         f.close()
-        
+
+    def test_ShouldPrintErrorAndClassAndMethodToFile(self):
+        self.testLogger.Error("This is an Error", "errorClass", "errorFunction")
+        f = open(self.errorFile, 'r')
+        self.assertEqual(f.readline(), "<---ERROR--->\n")
+        self.assertEqual(f.readline(), "This is an Error\n")
+        self.assertEqual(f.readline(), "In Class errorClass\n")
+        self.assertEqual(f.readline(), "In Function errorFunction\n")
+        self.assertEqual(f.readline(), "<---ERROR--->\n")
+        f.close()
+    
+    def test_ShouldPrintTraceLogsToFile(self):
+        self.testLogger.Trace("This is a trace message")
+        self.testLogger.Trace("This is another trace message")
+        f = open(self.traceFile, 'r')
+        self.assertEqual(f.readline(), "This is a trace message\n")
+        self.assertEqual(f.readline(), "This is another trace message\n")
+        f.close()
 
 if __name__ == "__main__":
     unittest.main()
