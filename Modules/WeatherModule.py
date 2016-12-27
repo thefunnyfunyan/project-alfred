24f208a075a3fb792805dc7057cd564e11f47873
Brandon Runyan <brandon.runyan@igtm.com>
2016-12-27 14:09:22 -0600

    started creating weather module

---
+++ Modules/WeatherModule.py
@@ -0,0 +1,9 @@
+import requests
+
+def main():
+  req = requests.get('https://api.darksky.net/forecast/80ae193703675485dfb06172c87a3911/39.188771, -96.549764')
+  reqJson = req.json()
+  print(reqJson['currently']['temperature'])
+
+if __name__ == '__main__':
+  main()
