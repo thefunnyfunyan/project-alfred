c8d57ab78c54ead582a84fde4226f878ebda75e1
Brandon Runyan <brandon.runyan@igtm.com>
2016-12-30 14:39:51 -0600

    basic api call implemented

--- Modules/WeatherModule.py
+++ Modules/WeatherModule.py
@@ -3,7 +3,7 @@ import requests
 def main():
   req = requests.get('https://api.darksky.net/forecast/80ae193703675485dfb06172c87a3911/39.188771, -96.549764')
   reqJson = req.json()
-  print(reqJson['currently']['temperature'])
+  print(reqJson['hourly']['data'][0])
 
 if __name__ == '__main__':
   main()
