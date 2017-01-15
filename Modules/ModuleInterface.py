36ce5de8ed6d0b0f6eb21ce52965a2a1bbdd48ea
Brandon Runyan <brandon.runyan15@gmail.com>
2017-01-15 14:18:24 -0600

    implemented basic summary information for current hour, day, and week

--- Modules/ModuleInterface.py
+++ Modules/ModuleInterface.py
@@ -5,7 +5,6 @@ class IModule(object):
   _keyWords = []
 
   def __init__(self, ioEngine: IOEngine):
-    assert isinstance(ioEngine, IOEngine)
     self.ioEngine = ioEngine
 
   def execute(self, wordList: [str]) -> None: raise NotImplementedError
--- Modules/ModuleList.py
+++ Modules/ModuleList.py
@@ -1,19 +1,21 @@
+from IOEngine.IOEngine import IOEngine
 from .TestModule import TestModule
 from .DefaultModule import DefaultModule
 from .ModuleInterface import IModule
-from IOEngine.IOEngine import IOEngine
 from .ExitModule import ExitModule
+from .WeatherModule import WeatherModule
 
 
 class ModuleList:
-  def getModules(ioEngine: IOEngine) -> [IModule]:
-    modList = [
-      ExitModule(ioEngine),
-      TestModule(ioEngine),
-      DefaultModule(ioEngine)
-    ]
-    return modList
+    def getModules(ioEngine: IOEngine) -> [IModule]:
+        modList = [
+            ExitModule(ioEngine),
+            TestModule(ioEngine),
+            WeatherModule(ioEngine),
+            DefaultModule(ioEngine)  # has to be last
+        ]
+        return modList
 
 
 if __name__ == "__main__":
-  None
+    None
