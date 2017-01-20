62dd8c932bd57d3906faa95219241e2fa4cc0f9d
Brandon Runyan <brandon.runyan15@gmail.com>
2017-01-20 12:32:11 -0600

    changed tab sizes

--- Modules/JokeModule.py
+++ Modules/JokeModule.py
@@ -3,38 +3,38 @@ from  IOEngine.IOEngine import IOEngine
 
 
 class Joke(object):
-  def tellJoke(self, ioEngine: IOEngine) -> None:
-    pass
+    def tellJoke(self, ioEngine: IOEngine) -> None:
+        pass
 
 
 class StoryJoke(Joke):
-  def __init__(self, story):
-    self._story = story
+    def __init__(self, story):
+        self._story = story
 
-  def tellJoke(self, ioEngine: IOEngine) -> None:
-    ioEngine.output(self._story)
+    def tellJoke(self, ioEngine: IOEngine) -> None:
+        ioEngine.output(self._story)
 
 
 class JokeModule(IModule):
-  _keyWords = ['joke', 'laugh', 'funny']
+    _keyWords = ['joke', 'laugh', 'funny']
 
-  def execute(self, wordList: [str]) -> None:
-    self.ioEngine.output('What kind of joke would you like to hear?')
-    userInput = self.ioEngine.getInput()
-    jokeType = self._getJokeTypeFromUserInput(userInput)
-    joke = self._getJoke(jokeType)
-    joke.tellJoke(self.ioEngine)
-    self.ioEngine.output('Would you like to hear another one?')
-    userInput = self.ioEngine.getInput()
-    if not self._shouldTellAnotherJoke(userInput):
-      return
+    def execute(self, wordList: [str]) -> None:
+        self.ioEngine.output('What kind of joke would you like to hear?')
+        userInput = self.ioEngine.getInput()
+        jokeType = self._getJokeTypeFromUserInput(userInput)
+        joke = self._getJoke(jokeType)
+        joke.tellJoke(self.ioEngine)
+        self.ioEngine.output('Would you like to hear another one?')
+        userInput = self.ioEngine.getInput()
+        if not self._shouldTellAnotherJoke(userInput):
+            return
 
-  def _getJokeTypeFromUserInput(self, userInput: [str]) -> str:
-    return 'story'
+    def _getJokeTypeFromUserInput(self, userInput: [str]) -> str:
+        return 'story'
 
-  def _getJoke(self, jokeType: str) -> Joke:
-    if (jokeType == 'story'):
-      return StoryJoke('Three men walk into a bar. The fourth one ducks')
+    def _getJoke(self, jokeType: str) -> Joke:
+        if (jokeType == 'story'):
+            return StoryJoke('Three men walk into a bar. The fourth one ducks')
 
-  def _shouldTellAnotherJoke(self, userInput: [str]) -> bool:
-    return False
+    def _shouldTellAnotherJoke(self, userInput: [str]) -> bool:
+        return False
