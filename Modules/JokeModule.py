8a244af1d4ec57cac6d92a4e0c60386950202e07
TheFunnyFunyan <brando15@ksu.edu>
2017-01-24 10:55:13 -0600

    basic joke functionality working

--- Modules/JokeModule.py
+++ Modules/JokeModule.py
@@ -2,48 +2,53 @@ from .ModuleInterface import IModule
 from  IOEngine.IOEngine import IOEngine
 from enum import Enum
 
+
 class JokeType(Enum):
     random = 0
     story = 1
     knock_knock = 2
 
+
 class Joke(object):
-    def tellJoke(self, iofEngine: IOEngine) -> None:
+    def tellJoke(self, io_engine: IOEngine) -> None:
         pass
+
 
 class StoryJoke(Joke):
     def __init__(self, story: str):
         self._story = story
 
-    def tellJoke(self, ioEngine: IOEngine) -> None:
-        ioEngine.output(self._story)
+    def tellJoke(self, io_engine: IOEngine) -> None:
+        io_engine.output(self._story)
+
 
 class KnockKnockJoke(Joke):
-    def __init__(self, setUp: str, punchLine: str):
-        self._setUp = setUp
-        self._punchLine = punchLine
+    def __init__(self, set_up: str, punch_line: str):
+        self._setUp = set_up
+        self._punchLine = punch_line
 
-    def tellJoke(self, ioEngine: IOEngine) -> None:
-        ioEngine.output('Knock knock')
-        ioEngine.getInput()
-        ioEngine.output(self._setUp)
-        ioEngine.getInput()
-        ioEngine.output(self._punchLine)
+    def tellJoke(self, io_engine: IOEngine) -> None:
+        io_engine.output('Knock knock')
+        io_engine.getInput()
+        io_engine.output(self._setUp)
+        io_engine.getInput()
+        io_engine.output(self._punchLine)
 
 
 class JokeModule(IModule):
     _keyWords = ['joke', 'laugh', 'funny']
 
-    def execute(self, wordList: [str]) -> None:
-        self.io_engine.output('What kind of joke would you like to hear?')
-        user_input = self.io_engine.getInput()
-        joke_type = self._getJokeTypeFromUserInput(user_input)
-        joke = self._getJoke(joke_type)
-        joke.tellJoke(self.io_engine)
-        self.io_engine.output('Would you like to hear another one?')
-        user_input = self.io_engine.getInput()
-        if not self._shouldTellAnotherJoke(user_input):
-            return
+    def execute(self, word_list: [str]) -> None:
+        while True:
+            self.io_engine.output('What kind of joke would you like to hear?')
+            user_input = self.io_engine.getInput()
+            joke_type = self._getJokeTypeFromUserInput(user_input)
+            joke = self._getJoke(joke_type)
+            joke.tellJoke(self.io_engine)
+            self.io_engine.output('Would you like to hear another one?')
+            user_input = self.io_engine.getInput()
+            if not self._shouldTellAnotherJoke(user_input):
+                return
 
     def _getJokeTypeFromUserInput(self, user_input: [str]) -> JokeType:
         story_keywords = ['story', 'long']
@@ -67,13 +72,18 @@ class JokeModule(IModule):
             1: JokeType.knock_knock
         }[final_index]
 
-    def _getJoke(self, jokeType: JokeType) -> Joke:
-        if (jokeType == JokeType.story):
+    def _getJoke(self, joke_type: JokeType) -> Joke:
+        if joke_type == JokeType.story:
             return StoryJoke('Three men walk into a bar. The fourth one ducks')
-        elif (jokeType == jokeType.knock_knock):
+        elif joke_type == joke_type.knock_knock:
             return KnockKnockJoke('boo', 'dont cry, its only a joke')
         else:
             return StoryJoke('random joke. haha')
 
-    def _shouldTellAnotherJoke(self, userInput: [str]) -> bool:
+    def _shouldTellAnotherJoke(self, user_input: [str]) -> bool:
+        _yes_words = ['yes', 'yea', 'sure', 'ok']
+        for input_word in user_input:
+            for word in _yes_words:
+                if input_word == word:
+                    return True
         return False
--- Modules/JokeModule__test.py
+++ Modules/JokeModule__test.py
@@ -5,29 +5,29 @@ from .JokeModule import JokeModule, StoryJoke, JokeType
 
 class JokeModuleTest(unittest.TestCase):
     def setUp(self):
-        self.ioEngine = IOEngineFake()
-        self.jokeModule = JokeModule(self.ioEngine)
+        self.io_engine = IOEngineFake()
+        self.joke_module = JokeModule(self.io_engine)
 
     def test_InitializationTest(self):
-        self.assertNotEqual(self.jokeModule, None)
+        self.assertNotEqual(self.joke_module, None)
 
     def test_ShouldGetStoryJokeType(self):
-        testInput = [['story'], ['long']]
-        for input in testInput:
-            self.assertEqual(self.jokeModule._getJokeTypeFromUserInput(input), JokeType.story)
+        test_input = [['story'], ['long']]
+        for input_word in test_input:
+            self.assertEqual(self.joke_module._getJokeTypeFromUserInput(input_word), JokeType.story)
 
     def test_ShouldReturnStoryJoke(self):
-        joke = self.jokeModule._getJoke(JokeType.story)
+        joke = self.joke_module._getJoke(JokeType.story)
         self.assertTrue(type(joke) is StoryJoke)
 
     def test_ShouldReturnTrueWhenGivenPositiveResponse(self):
-        self.assertTrue(self.jokeModule._shouldTellAnotherJoke([]))
+        self.assertTrue(self.joke_module._shouldTellAnotherJoke(['yea']))
 
     def test_ShouldGetKnockKnockJokeType(self):
-        self.assertEqual(self.jokeModule._getJokeTypeFromUserInput(['knock', 'knock', 'joke']), JokeType.knock_knock)
+        self.assertEqual(self.joke_module._getJokeTypeFromUserInput(['knock', 'knock', 'joke']), JokeType.knock_knock)
 
     def test_ShouldGetRandomJokeType(self):
-        self.assertEqual(self.jokeModule._getJokeTypeFromUserInput([]), JokeType.random)
+        self.assertEqual(self.joke_module._getJokeTypeFromUserInput([]), JokeType.random)
 
 class IOEngineFake(IOEngine):
     def __init__(self):
